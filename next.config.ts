7259cef09de83c544dfd911babd3da45af6e3507
karalumpas <karalumpas@gmail.com>
2025-08-30 13:12:48 +0200

    Fix Next.js image config: Allow external domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mgwp.medeland.dk',
+        port: '',
+        pathname: '/wp-content/uploads/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '9000',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '9000',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
